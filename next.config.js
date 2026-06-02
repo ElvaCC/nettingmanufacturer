fa3957b052e904e5e58195050bc253734ba6f2fa
ELVA <ELVA54710@GMAIL.COM>
2026-06-02 14:01:31 +0800

    add nettingfactory.com to image domains

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.nettingmanufacturer.com',
       },
+      {
+        protocol: 'https',
+        hostname: '**.nettingfactory.com',
+      },
     ],
   },
 };
